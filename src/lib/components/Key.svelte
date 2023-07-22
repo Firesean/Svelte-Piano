562f07bccac0cded58d3983fbac86af2e86d9600
Sean Morgan <Firesean98@gmail.com>
2023-07-22 02:07:29 -0700

    Adjusted keys for piano 'resizing'

--- src/lib/components/Key.svelte
+++ src/lib/components/Key.svelte
@@ -1,8 +1,11 @@
 <script>
     export let isSharp = false;
-
+    export let note = "C";
+    export let octaveGroup = 1;
+    export let keyOffset = 1;
 </script>
 
-<div class="{isSharp ? "bg-black w-[3rem] h-[10rem]" : "bg-white w-[4rem] h-[15rem]"} border-solid border-black border-2">
+<div class="{isSharp ? "bg-black w-[3rem] h-[10rem] absolute z-10" : "bg-white w-[5rem] h-[18rem] "} border-solid border-black border-2"
+style="{isSharp ? `left: ${5 * keyOffset}rem` : ""}">
 
 </div>
--- src/lib/components/Octave.svelte
+++ src/lib/components/Octave.svelte
@@ -1,16 +1,17 @@
 <script>
     import Key from "$lib/components/Key.svelte";
 </script>
-
-<Key /> <!-- C Flat -->
-<Key isSharp=true/> <!-- C Sharp -->
-<Key /> <!-- D Flat -->
-<Key isSharp=true/> <!-- D Sharp -->
-<Key /> <!-- E Flat -->
-<Key /> <!-- F Flat -->
-<Key isSharp=true/> <!-- F Sharp -->
-<Key /> <!-- G -->
-<Key isSharp=true/> <!-- G Sharp -->
-<Key /> <!-- A Flat -->
-<Key isSharp=true/> <!-- A Sharp -->
-<Key /> <!-- B Flat -->
+<div class="flex">
+    <Key /> <!-- C Flat -->
+    <Key isSharp=true keyOffset=.7/> <!-- C Sharp -->
+    <Key /> <!-- D Flat -->
+    <Key isSharp=true keyOffset=1.7/> <!-- D Sharp -->
+    <Key /> <!-- E Flat -->
+    <Key /> <!-- F Flat -->
+    <Key isSharp=true keyOffset=3.7/> <!-- F Sharp -->
+    <Key /> <!-- G -->
+    <Key isSharp=true keyOffset=4.7/> <!-- G Sharp -->
+    <Key /> <!-- A Flat -->
+    <Key isSharp=true keyOffset=5.7/> <!-- A Sharp -->
+    <Key /> <!-- B Flat -->
+</div>
